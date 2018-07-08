1bc58e1cae04eb462eaa778eec47dfd8c43cffd2
DaniCh <cd122011@gmail.com>
2018-07-08 16:49:46 +0300

    back button and label on top of settings activity (TODO: set edit Btn)

--- app/src/main/java/com/yoyoyo/danich/placeselecter/CountryList.kt
+++ app/src/main/java/com/yoyoyo/danich/placeselecter/CountryList.kt
@@ -1,9 +1,11 @@
 package com.yoyoyo.danich.placeselecter
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_list_of_countries.*
+import kotlinx.android.synthetic.main.lists_row.*
 
 class CountryList : AppCompatActivity() {
 
@@ -13,5 +15,10 @@ class CountryList : AppCompatActivity() {
 
         recyclerView_list.layoutManager = LinearLayoutManager(this)
         recyclerView_list.adapter = CountryAdapter()
+
+        /*editBtn.setOnClickListener{
+            val intent = Intent(this, CountryList::class.java)
+            startActivity(intent)
+        }*/
     }
 }
