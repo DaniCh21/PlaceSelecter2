1b136ecf8f1f8bbf392e4513ace82bdabba876a9
DaniCh <cd122011@gmail.com>
2018-07-09 13:56:15 +0300

    implemented: activity with european countries and displaying its size, added activity to manifest (TODO: remove or implement editBtn)

--- app/src/main/java/com/yoyoyo/danich/placeselecter/CountryList.kt
+++ app/src/main/java/com/yoyoyo/danich/placeselecter/CountryList.kt
@@ -1,11 +1,9 @@
 package com.yoyoyo.danich.placeselecter
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_list_of_countries.*
-import kotlinx.android.synthetic.main.lists_row.*
 
 class CountryList : AppCompatActivity() {
 
@@ -16,9 +14,5 @@ class CountryList : AppCompatActivity() {
         recyclerView_list.layoutManager = LinearLayoutManager(this)
         recyclerView_list.adapter = CountryAdapter()
 
-        /*editBtn.setOnClickListener{
-            val intent = Intent(this, CountryList::class.java)
-            startActivity(intent)
-        }*/
     }
 }
--- app/src/main/java/com/yoyoyo/danich/placeselecter/MainAdapter.kt
+++ app/src/main/java/com/yoyoyo/danich/placeselecter/MainAdapter.kt
@@ -1,10 +1,11 @@
 package com.yoyoyo.danich.placeselecter
 
+
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.countries_row.view.*
 import kotlinx.android.synthetic.main.lists_row.view.*
 
 
@@ -13,7 +14,11 @@ class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         val readyCountryListLabel = readyCountryListLabels.get(position)
         holder.view.listTitleTv.text = readyCountryListLabel
+        holder.view.countryAmountTv.text = europeCountries.size.toString()
     }
+
+    val europeCountries = arrayListOf("Andorra", "Austria", "Bulgaria", "Cyprus", "Czech Republic", "Finland", "France", "Georgia", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Norway", "Poland", "Portugal", "Slovakia", "Spain", "Sweden", "Switzerland", "Turkey", "Ukraine", "United Kingdom")
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -28,9 +33,13 @@ class MainAdapter: RecyclerView.Adapter<CustomViewHolder>() {
     }
 
 }
-
 class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view){
-
+    init {
+        view.setOnClickListener{
+            val intent = Intent(view.context, CountryList::class.java)
+            view.context.startActivity(intent)
+        }
+    }
 }
 
 
